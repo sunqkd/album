82752c52261ed6a8377fccf99e4a4df0194df28a
sunzhaoqkd <sunzhaoqkd@163.com>
2019-04-18 16:02:54 +0800

    update

--- src/components/globalSearch.vue
+++ src/components/globalSearch.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        全局搜索
+        <button @click="goback()">返回</button>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        goback(){
+            this.$router.back(-1);
+        }
+    }
 }
 </script>
