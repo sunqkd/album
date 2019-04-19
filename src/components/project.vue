959c2d9bd055452ed7d141d5ed00dd5cee426b2e
sunzhaoqkd <sunzhaoqkd@163.com>
2019-04-19 17:22:40 +0800

    update

--- src/components/project.vue
+++ src/components/project.vue
@@ -318,9 +318,9 @@
                 // console.log(albumId);
                 this.$router.push({path:'/globalSearch',query:{userId:this.userId,token:this.token,albumId:this.albumId}})
                 if(window.webkit){
-                    window.webkit.messageHandlers.addProject.postMessage({body: albumId});
+                    window.webkit.messageHandlers.addProject.postMessage({body: this.albumId});
                 }else{
-                    window.addProject.sendermsg(albumId);
+                    window.addProject.sendermsg(this.albumId);
                 }
             },
             manageProject(){ // 管理项目
