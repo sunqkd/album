21557e08494cf0d34aa169f95dbcb78514305aec
sunzhaoqkd <sunzhaoqkd@163.com>
2019-04-19 12:55:59 +0800

    update

--- src/components/globalSearch.vue
+++ src/components/globalSearch.vue
@@ -6,7 +6,7 @@
                 <div class="leftInput">
                     <img src="./img/search.png" alt="">
                 </div>
-                <input type="text"  class="inputSearch" v-model="keyWord" @keyup.enter="getSearchInfo()"/>
+                <input type="text"  class="inputSearch" v-model="keyWord" @keyup.enter.prevent="$event.target.blur" @blur="getSearchInfo()"/>
                 <div class="rightInput">
                     <img src="./img/slice.png" alt="" @click="cancelkeyWord()">
                 </div>
